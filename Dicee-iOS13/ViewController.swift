bdc4112d57e9eee8b7a3884fe3f7e6cb672b8f4f
guccibase <tahiru@tryhungry.com>
2020-06-02 09:13:36 -0400

    random int intro

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+ 
+
+    
+  
+
+    @IBAction func diceRollButton(_ sender: UIButton) {
+        
+        let num = Int.random(in: 0...5)
+         let num1 = Int.random(in: 0...5)
+        let dice = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image = dice[num]
+        diceImageView2.image = dice[num1]
+
+        
+
+        
+
+        
     }
-
-
+    
 }
 
